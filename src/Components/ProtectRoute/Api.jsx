cbe3f7edd1413092ad00eef7b79d85feddd28d57
Ibraheem Yasin Gill <113242732+devloper45@users.noreply.github.com>
2024-12-07 23:33:24 +0500

    Update Api.jsx

--- src/Components/ProtectRoute/Api.jsx
+++ src/Components/ProtectRoute/Api.jsx
@@ -1,15 +1,35 @@
-import React from "react";
 import axios from "axios";
 import { ApiBaseUrl } from "../../utils/util";
 
-const token = localStorage.getItem("userID");
-console.log("token hello" + token);
+const getToken = () => localStorage.getItem("userID");
 
 const Api = axios.create({
   baseURL: ApiBaseUrl,
   headers: {
-    authorization: `Bearer ${token}`,
+    authorization: Bearer ${getToken()},
   },
 });
+
+
+Api.interceptors.request.use((config) => {
+  const token = getToken();
+  if (token) {
+    config.headers.authorization = Bearer ${token};
+  }
+  return config;
+});
+
+
+Api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      console.error("Token expired or unauthorized. Redirecting to login...");
+      localStorage.removeItem("userID");
+      window.location.href = "/login"; 
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default Api;
