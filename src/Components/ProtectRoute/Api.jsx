629f59e836d4c89122c9baa40b8b3f2415de260c
devloper45 <ibraheemyasin23@gmail.com>
2024-12-07 23:58:20 +0500

    g

--- src/Components/ProtectRoute/Api.jsx
+++ src/Components/ProtectRoute/Api.jsx
@@ -1,3 +1,7 @@
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 import axios from "axios";
 import { ApiBaseUrl } from "../../utils/util";
 
@@ -10,6 +14,10 @@ const Api = axios.create({
   },
 });
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 Api.interceptors.request.use((config) => {
   const token = getToken();
   if (token) {
@@ -18,13 +26,21 @@ Api.interceptors.request.use((config) => {
   return config;
 });
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 Api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response && error.response.status === 401) {
       console.error("Token expired or unauthorized. Redirecting to login...");
       localStorage.removeItem("userID");
+<<<<<<< Updated upstream
       window.location.href = "/login";
+=======
+      window.location.href = "/login"; 
+>>>>>>> Stashed changes
     }
     return Promise.reject(error);
   }
